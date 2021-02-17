5eeb7d37a5446a333fbe1686165594a24e565c1a
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-17 15:59:58 +0300

    2. Каркас шахматной доски [#353387] Тесты

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -3,24 +3,32 @@ package ru.job4j.chess.firuges.black;
 import org.junit.Test;
 import ru.job4j.chess.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static ru.job4j.chess.firuges.Cell.*;
 
 public class BishopBlackTest {
 
     @Test
     public void position() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        BishopBlack bishopBlack = new BishopBlack(C1);
         bishopBlack.position();
+        assertThat(bishopBlack.position(), is(C1));
     }
 
     @Test
     public void copy() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        bishopBlack.copy(Cell.C1);
+        BishopBlack bishopBlack = new BishopBlack(C1);
+        Figure figure = bishopBlack.copy(C1);
+        assertThat(figure.position(), is(bishopBlack.position()));
     }
 
     @Test
     public void way() throws ImpossibleMoveException {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        bishopBlack.way(Cell.G5);
+        BishopBlack bishopBlack = new BishopBlack(C1);
+        Cell[] cells = new Cell[] {D2, E3, F4, G5, H6};
+        assertThat(bishopBlack.way(H6), is(cells));
     }
 }
